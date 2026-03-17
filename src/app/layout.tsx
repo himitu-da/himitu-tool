bb0dbe78a2b7daa987da00ddb3284575e3b25f9c
himitu-da <0127himitu@gmail.com>
2026-03-17 23:46:52 +0900

    feat: Add .htaccess for URL rewriting and cache control settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
         <Script
           strategy="afterInteractive"
           src={`https://www.googletagmanager.com/gtag/js?id=G-7YK9QB157S`}
