75445d22ed5d858fffec39715b845285ec9e34ab
himitu-da <0127himitu@gmail.com>
2026-03-19 12:17:10 +0900

    change to UTF-8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-﻿import type { Metadata } from "next";
+import type { Metadata } from "next";
 import Script from "next/script";
 import { ThemeProvider } from "./ThemeProvider";
 import { GlobalHeader } from "./GlobalHeader";
